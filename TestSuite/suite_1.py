96e27ca8e34f1f7a727202f223f9531cbb56be45
Amany-Moustafa <amany.af@gmail.com>
2016-01-06 10:48:13 +0200

    commit new test case it test suite_1

--- TestSuite/suite_1.py
+++ TestSuite/suite_1.py
@@ -3,16 +3,18 @@ import sys
 from TestCases.login_unittest import TestLogin
 from TestCases.createNewAccount_unittest import TestCreateNewAccount
 from BaseObjects.basetestcase import BaseTestCase
+from TestCases.MainPage_unittest import MainPageTest
 
 class TestSuite(BaseTestCase):
 
      def test_main(self):
         # suite of TestCases
-        # 
+        #
         self.suite = unittest.TestSuite()
         self.suite.addTests([
             unittest.defaultTestLoader.loadTestsFromTestCase(TestLogin.testLogin),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestCreateNewAccount.test_CreateNewAccount),
+            unittest.defaultTestLoader.loadTestsFromTestCase(MainPageTest.test_checklogo_displayed(self))
 
             ])
         runner = unittest.TextTestRunner()
